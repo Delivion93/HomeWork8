41f8c157f7577df0dd0163e1b4d2edc8985ea10b
Delivion93 <artem.abramov.1993@gmail.com>
2023-09-04 21:37:59 +0200

    change ShapeNamePrinter

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import Shapes.*;
 public class Main {
     public static void main(String[] args) {
-        
+
+        Square square = new Square(5);
+        ShapeNamePrinter.printShapeName(square);
+
     }
 }
